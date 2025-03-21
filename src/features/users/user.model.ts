85d3a71832cf103fbb9fbf2a81c018ed05549833
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-03-21 15:50:19 +0000

    JWT helper functions

--- src/features/users/user.model.ts
+++ src/features/users/user.model.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Model, ObjectId } from "mongoose";
 
-interface IUser extends Document {
+export interface IUser extends Document {
   _id: mongoose.Types.ObjectId;
   email: string;
   password?: string;
